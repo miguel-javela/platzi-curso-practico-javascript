e60669c7e7120a8c2c4c62498b15d946f1b58fdd
miguel javela <migueljavela2014@gmail.com>
2022-04-03 14:41:34 -0500

    promedio

---
+++ promedio.js
@@ -0,0 +1,16 @@
+const lista1 = [100,200,300,500]
+
+function calcularPromedio(lista){
+    // let suma =0 ;
+    // for (let elemento of lista){
+    //     suma += elemento
+    // }
+    // const promedio = suma/lista.length;
+    // return promedio;
+
+    const suma = lista.reduce(
+        function (valorAcumulado = 0, elementoActual){
+            return valorAcumulado + elementoActual;
+        }
+    )
+}
