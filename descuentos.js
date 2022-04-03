dc94b005f4e4b6c1428814deb062e9f6dee7ab46
miguel javela <70237877+migueljav13@users.noreply.github.com>
2022-04-03 14:09:08 -0500

    HTML con JS descuentos

--- descuentos.js
+++ descuentos.js
@@ -3,3 +3,12 @@ function CalcularPrecioConDescuento(precio,descuento){
     const precioConDescuento = (precio*porcenPrecioConDescuento)/100;
     return precioConDescuento
 };
+
+function calcularPrecio(){
+    const price = document.getElementById("price").value;
+    const discount = document.getElementById("discount").value;
+    precioConDescuento=CalcularPrecioConDescuento(price,discount)
+
+    parrafo = document.getElementById("result");
+    parrafo.innerText = "precio con descuento " + precioConDescuento 
+}
