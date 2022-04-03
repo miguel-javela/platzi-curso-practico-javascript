4e3b14cd9e4caa517bd4767e6b525b17445ec293
miguel javela <70237877+migueljav13@users.noreply.github.com>
2022-04-03 12:20:53 -0500

    se conecta HTML con JS para calcular perimetro y area

--- 5-figuras.js
+++ 5-figuras.js
@@ -16,10 +16,10 @@ console.group("END")
 console.group("TRIANGULO")
 
 function perimetroTriangulo (lado1,lado2,base){
-    return lado1+lado2+base
+    return lado1+lado2+base;
 }
 function areaTriangulo(base,altura){
-    return base*altura/2
+    return base*altura/2;
 }
 
 console.group("END")
@@ -45,3 +45,51 @@ function areaCirculo (radio){
 }
 
 console.group("END")
+
+//funciones para HTML
+
+//CUADRADO
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+function calcularAreaCuadrado(){
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const perimetro = areaCuadrado(value);
+    alert(perimetro);
+}
+
+//TRIANGULO
+function calcularPerimetroTriangulo(){
+    const lado1 = document.getElementById("lado1").value;
+    const lado2 = document.getElementById("lado2").value;
+    const base = document.getElementById("base").value;
+    const lado1N = Number(lado1);
+    const lado2N = Number(lado2);
+    const baseN = Number(base);
+    const perimetro = perimetroTriangulo(lado1N,lado2N,baseN);
+    alert(perimetro);
+}
+function calcularAreaTriangulo(){
+    const base = document.getElementById("base").value;
+    const altura = document.getElementById("altura").value;
+    const baseN = Number(base);
+    const alturaN = Number(altura);
+    const area = areaTriangulo(baseN,alturaN);
+    alert (area);
+}
+
+//CIRCULO
+function calcularPerimetroCirculo(){
+    const radio = document.getElementById("radio").value;
+    const perimetro = perimetroCirculo(radio);
+    alert(perimetro);
+}
+function calcularAreaCirculo(){
+    const radio = document.getElementById("radio").value;
+    const area = areaCirculo(radio);
+    alert (area);
+}
